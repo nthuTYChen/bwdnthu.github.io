0a119c44e4a2935c048a887cbc547129ae05cd1d
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2020-12-18 13:04:54 +0800

    20201218

---
+++ bwd/quizzes/4-1/4-1.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('button').eq(0).on('click', function () {
+		$('div').css({
+			'height': '50px',
+			'width': '50px'
+		});
+		$('div:nth-of-type(odd)').css({
+			'height': '100px',
+			'width': '100px'
+		});
+		//$('div').removeClass('selected');
+		//$('div:nth-of-type(odd)').addClass('selected');
+	});
+	$('button').eq(1).on('click', function () {
+		$('div').removeClass('selected');
+		$('div:nth-of-type(even)').addClass('selected');
+	});
+	$('button').eq(2).on('click', function () {
+		$('div').removeClass('selected');
+	});
+});
