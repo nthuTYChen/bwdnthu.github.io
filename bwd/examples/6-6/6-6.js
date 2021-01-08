b5444a5a6a8a6ce1faef314be341ce608106eb21
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2021-01-08 13:11:53 +0800

    20210108

---
+++ bwd/examples/6-6/6-6.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#hideDiv').on('click', function () {
+		$('div').hide('fast');
+	});
+
+	$('#showDiv').on('click', function() {
+		$('div').show('slow');
+	});
+});
