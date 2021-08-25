08ff6aaa62f463cb3da1d6865820b1f2faf24e76
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2021-08-25 21:53:15 +0800

    2021 Fall Update
    Essay analysis

--- bwd/analysisdemo/demo.js
+++ bwd/analysisdemo/demo.js
@@ -1,27 +1,36 @@
-var menuOpen = false;
+var menuOpen = false, touchMove = false;
 
 $(document).ready(()=>{
+	$(document).on('touchmove', ()=>{
+		touchMove = true;
+		setTimeout(()=>{
+			touchMove = false;
+		}, 500);
+	});
+
 	$('nav > a:first-of-type').on('click touchend', (event)=>{
 		event.stopPropagation();
-		let menuItems = $('nav > a').not(':first-of-type');
-		let delay = 0;
-		if(!menuOpen) {
-			menuItems.each((index)=>{
-				setTimeout(()=>{
-					menuItems.eq(index).css('display', 'grid');
-					menuItems.eq(index).animate({opacity: 0.95}, 300);
-				}, delay + (50 * index));
-			});
+		if(!touchMove) {
+			let menuItems = $('nav > a').not(':first-of-type');
+			let delay = 0;
+			if(!menuOpen) {
+				menuItems.each((index)=>{
+					setTimeout(()=>{
+						menuItems.eq(index).css('display', 'grid');
+						menuItems.eq(index).animate({opacity: 0.95}, 300);
+					}, delay + (50 * index));
+				});
+			}
+			else {
+				menuItems.each((index)=>{
+					setTimeout(()=>{
+						menuItems.eq(index).animate({opacity: 0}, 300, ()=>{
+							menuItems.eq(index).css('display', 'none');
+						});
+					}, delay + (50 * index));
+				});
+			}
+			menuOpen = !menuOpen;
 		}
-		else {
-			menuItems.each((index)=>{
-				setTimeout(()=>{
-					menuItems.eq(index).animate({opacity: 0}, 300, ()=>{
-						menuItems.eq(index).css('display', 'none');
-					});
-				}, delay + (50 * index));
-			});
-		}
-		menuOpen = !menuOpen;
 	});
 });
